4351484729d7d697763ae48663ce03959bc2c609
xutaoding <xutao.ding@chinascopefinancial.com>
2016-03-11 11:03:23 +0800

    modify BaseCommonSpider class and handle config

--- news_spiders/spiders/common.py
+++ news_spiders/spiders/common.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from scrapy import Spider, Selector
 from scrapy import Request
 
@@ -9,48 +10,40 @@ from ..utils.utils import populate_md5, deepcopy
 
 
 class Collector(object):
-    config_instance = _InitConfigs()
+    configs = {}
+    start_urls = defaultdict(list)
+
+    def __init__(self):
+        self.__config_instance = _InitConfigs()
+        self.__init_config()
 
     def unique_name(self, name):
-        return name in self.config_instance.names
+        return name in self.__config_instance.names
 
-    def get_start_urls(self):
+    def __init_config(self):
         """ get total url as start_urls to offer Spiders, then populate related site configs """
-        _configs = {}
-        _start_urls = []
-
-        required_configs = self.config_instance.total_configs
+        required_configs = self.__config_instance.total_configs
 
         for _config in required_configs:
             site_name = _config['site']
             base_conf = {_key: _value for _key, _value in _config.iteritems() if _key != 'urls'}
 
             for url_conf in _config['urls']:
-                cate = url_conf['cate']
                 base_url = url_conf['page_url']
                 url_fill_rule = url_conf['first']
                 start_page = url_conf.get('start', 1)
-                total_page = url_conf.get('pages', 1) + 1
 
-                while start_page < total_page:
-                    # Deal with turning page to site link
-                    # Populate config to each page url
-                    required_url = PageUri(
-                        site_name=site_name,
-                        base_url=base_url,
-                        url_fill_rule=url_fill_rule,
-                        page=start_page
-                    ).get_page_url()
+                while start_page < url_conf.get('pages', 1) + 1:
+                    # Deal with turning page to site link, and Populate config to each page url
+                    required_url = PageUri(site_name, base_url, url_fill_rule, start_page).get_page_url()
 
                     _conf_key = populate_md5(required_url)
-                    _start_urls.append(required_url)
-                    _configs[_conf_key] = deepcopy(base_conf)
-                    _configs[_conf_key]['cate'] = cate
+                    self.start_urls[site_name].append(required_url)
+                    self.configs[_conf_key] = deepcopy(base_conf)
+                    self.configs[_conf_key].update(cate=url_conf['cate'])
 
                     start_page += 1
                     url_fill_rule = url_conf['suffix'] or url_fill_rule
-
-        return _start_urls, _configs
 
 
 class BaseCommonSpider(Spider):
@@ -59,10 +52,10 @@ class BaseCommonSpider(Spider):
     """
     name = 'news_spiders'
     collector = Collector()
-    start_urls, config = collector.get_start_urls()
 
     def __init__(self, name=None, url=None, **kwargs):
         self.start_urls = []
+        self.config = self.collector.configs
 
         if name is None and url is None:
             # crawl config of all site web, then get total urls as start_urls
@@ -80,23 +73,18 @@ class BaseCommonSpider(Spider):
         super(BaseCommonSpider, self).__init__(name=self.name, **kwargs)
 
     def _start_urls(self, name=None):
-        class_start_urls = self.__class__.start_urls
-        urls_mapping = {populate_md5(_url): _url for _url in class_start_urls}
+        start_urls = self.collector.start_urls
 
-        if name is None:
-            self.start_urls = class_start_urls
-            return
-
-        for _url_md5, _config in self.config.iteritems():
-            site_name = _config['site']
-            if name == site_name:
-                self.start_urls.append(urls_mapping[_url_md5])
+        if name is not None:
+            self.start_urls.extend([_url for _su in start_urls[name] for _url in _su])
+        else:
+            self.start_urls.extend([_url for _su in start_urls.itervalues() for _url in _su])
 
     def parse(self, response):
         conf_value = populate_md5(response.url)
         total_urls = UrlsResolver(
             selector=Selector(response),
-            config=self.config[conf_value],
+            config=self.config[conf_value]
         ).resolve()
 
         for _each_url in total_urls:
