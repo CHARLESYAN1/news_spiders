80b9db13a1e3f811d3d368a458ede3d0b0ef4e23
xutaoding <xutao.ding@chinascopefinancial.com>
2016-03-24 19:09:49 +0800

    remove redis_cached vairable

--- news_spiders/contrib/__init__.py
+++ news_spiders/contrib/__init__.py
@@ -1,17 +1,9 @@
-from ..conf import news_config
 from .aws.bucket import Bucket
 from .pyredis.base import Base as RedisBase
 from .pyredis.cached import RedisCached
 from .pyredis.convert import PickleToQueue
 from .pyredis.convert import UnpickleToFile
 from .transfer.goosy import GoosyTransfer
-
-
-if news_config.settings['PLATFORM']:
-    redis_cached = set()
-    print 'redis_cached:', redis_cached, id(redis_cached)
-else:
-    redis_cached = RedisCached().get()
 
 __all__ = ['Bucket', 'RedisCached', 'GoosyTransfer', 'PickleToQueue', 'UnpickleToFile',
            'GoosyTransfer', 'RedisBase']
