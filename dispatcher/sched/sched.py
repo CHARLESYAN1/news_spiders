31249bb3ebf23e433f7b51b325f39c66cd5b7966
xutaoding <xutao.ding@chinascopefinancial.com>
2016-03-31 18:28:17 +0800

    fix log and test

--- dispatcher/sched/sched.py
+++ dispatcher/sched/sched.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os.path
+from os.path import abspath as _abs
 
 from .. import app, logger
 from .base import BaseSched, Intervals
@@ -14,24 +14,30 @@ def dispatch_full_jobs():
     if bs.is_migrate is None:
         return
 
+    interval_sites = []
+    remain_sites = []
     most_sites = bs.most_sites
     intervals = Intervals().intervals
+
     try:
         for site, interval_type_dict in intervals.iteritems():
             for type_key, interval in interval_type_dict.iteritems():
                 kw_values = most_sites[site]
                 bs.dispatch_job(type_key, interval, kw_values)
+                interval_sites.append('{}:<{}>'.format(site, kw_values))
 
         rest_sites_keys = set(most_sites.keys()) - set(intervals.keys())
 
         for _rest_keys in rest_sites_keys:
             # 将剩下的网站按照三个时间段分开添加任务
-            bs.dispatch_job(1, 5, most_sites[_rest_keys])
-            bs.dispatch_job(2, 8, most_sites[_rest_keys])
-            bs.dispatch_job(3, 10, most_sites[_rest_keys])
+            sites_of_related = most_sites[_rest_keys]
+
+            bs.dispatch_job(1, 5, sites_of_related)
+            bs.dispatch_job(2, 2, sites_of_related)
+            bs.dispatch_job(3, 10, sites_of_related)
+            remain_sites.append('{}:<{}>'.format(_rest_keys, sites_of_related))
     except Exception as e:
-        logger.info('Dispatch full jobs error: type <{}>, msg <{}>, file <{}>'.format(
-            e.__class__, e, os.path.abspath(__file__)))
+        logger.info('Dispatch full jobs error: type <{}>, msg <{}>, file <{}>'.format(e.__class__, e, _abs(__file__)))
 
 
 @app.scheduled_job(trigger='interval', minutes=2, seconds=30,)
@@ -44,8 +50,7 @@ def dispatch_hot_jobs():
     try:
         bs.schedule(site_name=bs.hot_sites)
     except Exception as e:
-        logger.info('Dispatch hot jobs error: type <{}>, msg <{}>, file <{}>'.format(
-            e.__class__, e, os.path.abspath(__file__)))
+        logger.info('Dispatch hot jobs error: type <{}>, msg <{}>, file <{}>'.format(e.__class__, e, _abs(__file__)))
 
 
 @app.scheduled_job(trigger='interval', minutes=4)
@@ -58,8 +63,7 @@ def dispatch_sgp_jobs():
     try:
         bs.schedule(site_name=bs.sgp_sites)
     except Exception as e:
-        logger.info('Dispatch Sgp jobs error: type <{}>, msg <{}>, file <{}>'.format(
-            e.__class__, e, os.path.abspath(__file__)))
+        logger.info('Dispatch Sgp jobs error: type <{}>, msg <{}>, file <{}>'.format(e.__class__, e, _abs(__file__)))
 
 
 @app.scheduled_job(trigger='cron', hour='0', minute='0', second='0')
