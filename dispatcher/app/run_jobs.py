269a6df26787cfa50361e4650a3e297701654e89
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-05 16:21:14 +0800

    test

--- dispatcher/app/run_jobs.py
+++ dispatcher/app/run_jobs.py
@@ -7,7 +7,7 @@ sys.path.append(dirname(dirname(dirname(abspath(__file__)))))
 from dispatcher.app import *
 
 
-@app.scheduled_job(trigger='interval', seconds=10)
+@app.scheduled_job(trigger='interval', minutes=1)
 def pprint_jobs():
     schedule_jobs = 0
     other_jobs = 0
@@ -17,8 +17,8 @@ def pprint_jobs():
             schedule_jobs += 1
         else:
             other_jobs += 1
-    print 'Schedule jobs:', schedule_jobs
-    print 'Other jobs:   ', other_jobs
+    print 'Schedule jobs:', schedule_jobs,
+    print 'Other jobs:   ', other_jobs,
     print 'Total jobs:   ', len(app.get_jobs())
     print 'Now:', datetime.now(), '\n', '_' * 50, '\n'
 
