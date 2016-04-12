8ffcc13bec98dc9c233ee02505e0d2fae64ab3d1
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-12 12:03:32 +0800

    get exception info

--- news_spiders/exceptions/__init__.py
+++ news_spiders/exceptions/__init__.py
@@ -1,3 +1,7 @@
+import sys
+import linecache
+
+
 class NotExistFileError(Exception):
     pass
 
@@ -5,3 +9,17 @@ class NotExistFileError(Exception):
 class NotExistSiteError(Exception):
     pass
 
+
+def get_exce_info():
+    exc_type, exc_value, tb = sys.exc_info()
+    frame = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = frame.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, frame.f_globals)
+    return 'Type: [{type}], Info: [{value}], Path: [{file_path}], LineNo: [{lineno}]'.format(
+        type=exc_type,
+        value=line.strip(),
+        file_path=filename,
+        lineno=lineno
+    )
