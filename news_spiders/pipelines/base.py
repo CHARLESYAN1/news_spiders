fce0f0f159c4158c7b15cd22f75a98ec816288c9
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-19 18:08:06 +0800

    insert mongo on test dev

--- news_spiders/pipelines/base.py
+++ news_spiders/pipelines/base.py
@@ -56,7 +56,7 @@ class Base(object):
 
     def insert2mongo(self, data):
         try:
-            if self.is_migrate is True:
+            if self.is_migrate in [True, False]:
                 data['d'] = data['dt'][:8]
                 self.mongo.insert(data)
         except (TimeoutError, DuplicateKeyError, ExceededMaxWaiters, AutoReconnect) as e:
