8b0fcc4e970779292b3a240cd4127046c5c8a081
xutaoding <xutao.ding@chinascopefinancial.com>
2016-06-30 13:29:39 +0800

    挑选一些关于手机展示良好，版权优先， 数量多的网站；修改文档名称

---
+++ dispatcher/sched/priority.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Mobile展示较好，且数量优先
+SITES_PRIORITY = [
+    ('10jqka', '同花顺'),
+    ('jrj', '金融界'),
+    ('sina', '新浪财经'),
+    ('eastday', '东方网'),
+    ('cnstock', '中国证券网'),
+    ('p5w', '全景网'),
+    ('sohu', '搜狐财经'),
+    ('ifeng', '凤凰财经'),
+    ('yicai', '一财网'),
+    ('cs', '中证网'),
+    ('fx168', 'FX168财经'),
+    ('xinhuanet', '新华网'),
+    ('stcn', '证券时报'),
+    ('jiemian', '界面'),
+    ('wallstreetcn', '华尔街见闻'),
+    ('qq', '腾讯财经'),
+    ('163', '网易财经'),
+    ('hexun', '和讯网'),
+    ('thepaper', '澎湃新闻'),
+    ('ccstock', '中国资本证券网'),
+    ('kxt', '快讯通财经'),
+    ('emoney', '益盟财经'),
+    ('caixin', '财新网'),
+    ('chinaipo', '新三板在线'),
+    ('takungpao', '大公报'),
+    ('xinhua08', '中国金融信息网'),
+    ('cnfol', '中金在线'),
+    ('huaxi100', '华西新闻'),
+    ('ftchinese', 'FT中文网'),
+
+]
+
+# 版权优先，且数量优先
+COPYRIGHT = [
+    ('cnstock', '中国证券网'),
+    ('yicai', '一财网'),
+    ('stcn', '证券时报'),
+    ('jiemian', '界面'),
+    ('thepaper', '澎湃新闻'),
+    ('wallstreetcn', '华尔街见闻'),
+    ('ccstock', '中国资本证券网'),
+    ('kxt', '快讯通财经'),
+    ('huaxi100', '华西新闻'),
+]
