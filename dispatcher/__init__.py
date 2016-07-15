7fc5c8ac516926c3a7c2b5b09d987e2ad5235157
xutaoding <xutao.ding@chinascopefinancial.com>
2016-07-15 11:34:02 +0800

    使用sqlite作为任务存储

--- dispatcher/__init__.py
+++ dispatcher/__init__.py
@@ -1,4 +1,8 @@
+import os
+from os.path import dirname, abspath
+
 from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
 
@@ -6,8 +10,18 @@ from news_spiders.utils import Logger
 
 logger = Logger('dispatch_jobs')
 
+
+def create_jobstrore_sqlite():
+    sqlite_path = dirname(abspath(__file__))
+    for sql_path in os.listdir(sqlite_path):
+        if sql_path.endswith('.db'):
+            os.remove(os.path.join(sqlite_path, sql_path))
+
+create_jobstrore_sqlite()
+
 jobstores = {
-    'default': MemoryJobStore()
+    # 'default': MemoryJobStore(),
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.db')
 }
 
 # using ThreadPoolExecutor as default other than ProcessPoolExecutor(not work) to executors
